5aa8a338ca57bde5ee022ac3e73d3ae511ce00b3
angelHG1 <angelhilario2019@hotmail.com>
2026-07-26 18:48:47 -0400

    Imagenes de ejercicios agregadas

--- obj/Debug/net10.0-windows/forms.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/forms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("forms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7725fe4ed821b32ad6ee21e7f2b13bee5d2673b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c9f32db36386209f00c7ca90b3f9f92b08ab30b")]
 [assembly: System.Reflection.AssemblyProductAttribute("forms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("forms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
