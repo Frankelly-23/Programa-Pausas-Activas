b270b41676472da328e879d774b83eccf910e8ee
Lusiany Castillo <Lusianycastillo@gmail.com>
2026-07-29 23:45:58 -0400

    .

--- obj/Debug/net10.0-windows/forms.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/forms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("forms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0c7e9558ba3c340776c7453d9e17f4bf8a3954c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc1b32db1b2f1650bfc162ad2acc1f5dae9b9f70")]
 [assembly: System.Reflection.AssemblyProductAttribute("forms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("forms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
