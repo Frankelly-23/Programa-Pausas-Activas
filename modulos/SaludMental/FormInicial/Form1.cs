a91187586b92b648aa20aa771c188bb6f1937c85
katy <linitapausita5576@gmail.com>
2026-07-31 16:47:38 -0400

    Listo

---
+++ modulos/SaludMental/FormInicial/Form1.cs
@@ -0,0 +1,36 @@
+namespace PausasActivas.Modulos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            FormRelajacion relajacion = new FormRelajacion();
+            relajacion.Show();
+            this.Hide();
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            FormEnfoque enfoque = new FormEnfoque();
+            enfoque.Show();
+            this.Hide();
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            FormRecuperacion recuperacion = new FormRecuperacion();
+            recuperacion.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
